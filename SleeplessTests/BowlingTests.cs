5d0365db95a9c36ddbc351128a7b11b9b549b205
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2017-01-15 02:38:26 -0600

    Score_depends_on_pins_rolled and add rollMany

--- SleeplessTests/BowlingTests.cs
+++ SleeplessTests/BowlingTests.cs
@@ -18,14 +18,19 @@ namespace SleeplessTests
             g = new BowlingGame();
         }
 
+        private void RollMany(int pins, int times)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+
         [Fact]
         public void Rolling_all_misses_scores_zero()
         {
             SetUp();
-            for (var i = 0; i < 20; i++)
-            {
-                g.Roll(0);
-            }
+            RollMany(0,20);
             g.Score().Should().Be(0);
         }
 
@@ -33,11 +38,19 @@ namespace SleeplessTests
         public void Rolling_all_ones_scores_20()
         {
             SetUp();
-            for (var i = 0; i < 20; i++)
-            {
-                g.Roll(1);
-            }
+            RollMany(1,20);
             g.Score().Should().Be(20);
         }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 20)]
+        public void Score_depends_on_pins_rolled(int pins, int expectedScore)
+        {
+            SetUp();
+            RollMany(pins,20);
+            g.Score().Should().Be(expectedScore);
+        }
+
     }
 }
