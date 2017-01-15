3bac39ecb731bb269ba3f7514ea016c9f8ad0b85
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2017-01-15 02:32:04 -0600

    Rolling_all_ones_scores_20

---
+++ Sleepless/BowlingGame.cs
@@ -0,0 +1,16 @@
+﻿namespace Sleepless
+{
+    public class BowlingGame
+    {
+        private int TrackedScore;
+        public void Roll(int pins)
+        {
+            TrackedScore = TrackedScore + pins;
+        }
+
+        public int Score()
+        {
+            return TrackedScore;
+        }
+    }
+}
---
+++ SleeplessTests/BowlingTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Sleepless;
+using Xunit;
+
+namespace SleeplessTests
+{
+    public class BowlingTests
+    {
+        private BowlingGame g;
+        protected void SetUp()
+        {
+            g = new BowlingGame();
+        }
+
+        [Fact]
+        public void Rolling_all_misses_scores_zero()
+        {
+            SetUp();
+            for (var i = 0; i < 20; i++)
+            {
+                g.Roll(0);
+            }
+            g.Score().Should().Be(0);
+        }
+
+        [Fact]
+        public void Rolling_all_ones_scores_20()
+        {
+            SetUp();
+            for (var i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+            }
+            g.Score().Should().Be(20);
+        }
+    }
+}
