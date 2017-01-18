b7d3dbf549bfabeb0d5287367c37ade6a0c77bb8
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2017-01-18 00:43:39 -0600

    Refactor existing tests

--- Sleepless.Tests/BowlingGameTests.cs
+++ Sleepless.Tests/BowlingGameTests.cs
@@ -10,26 +10,34 @@ namespace Sleepless.Tests
 {
     public class BowlingGameTests
     {
+        private BowlingGame _myGame;
+        private BowlingGame GetNewGame()
+        {
+            return new BowlingGame();
+        }
+
+        private void RollMany(int pins, int times)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                _myGame.Roll(pins);
+            }   
+        }
+
         [Fact]
         public void Rolling_all_misses_Scores_zero()
         {
-            BowlingGame newGame = new BowlingGame();
-            for (var i = 0; i < 20; i++)
-            {
-                newGame.Roll(0);
-            }
-            newGame.Score().Should().Be(0);
+            _myGame = GetNewGame();
+            RollMany(0,20);
+            _myGame.Score().Should().Be(0);
         }
 
         [Fact]
         public void Rolling_all_ones_Scores_20()
         {
-            BowlingGame newGame = new BowlingGame();
-            for (var i = 0; i < 20; i++)
-            {
-                newGame.Roll(1);
-            }
-            newGame.Score().Should().Be(20);
+            _myGame = GetNewGame();
+            RollMany(1,20);
+            _myGame.Score().Should().Be(20);
         }
     }
 }
