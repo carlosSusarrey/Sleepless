396ee8967feeaaeca2c440907e8d21e1575b5ab1
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2017-01-18 00:36:54 -0600

    Rolling_all_misses_Scores_zero

--- Sleepless.Tests/BowlingGameTests.cs
+++ Sleepless.Tests/BowlingGameTests.cs
@@ -3,7 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace Sleepless.Tests
 {
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void Rolling_all_misses_Scores_zero()
+        {
+            BowlingGame newGame = new BowlingGame();
+            for (var i = 0; i < 20; i++)
+            {
+                newGame.Roll(0);
+            }
+            newGame.Score().Should().Be(0);
+        }
+    }
 }
--- Sleepless/BowlingGame.cs
+++ Sleepless/BowlingGame.cs
@@ -1,57 +1,15 @@
-﻿namespace Sleepless
+namespace Sleepless
 {
     public class BowlingGame
     {
-        private int[] _rolls = new int[21];
-        private int _currentRoll = 0;
-
         public void Roll(int pins)
         {
-            _rolls[_currentRoll++] = pins;
+            
         }
 
         public int Score()
         {
-            var score = 0;
-            var frameIndex = 0;
-            for (var frame = 0; frame < 10; frame ++)
-            {
-                if (IsStrike(frameIndex))
-                {
-                    score = score + 10 + StrikeBonus(frameIndex);
-                }else if (IsSpare(frameIndex))
-                {
-                    score = score + 10 + SpareBonus(frameIndex);
-                    frameIndex ++;
-                }
-                else
-                {
-                    score = score + _rolls[frameIndex] + _rolls[frameIndex + 1];
-                    frameIndex ++;
-                }
-                frameIndex++;
-            }
-            return score;
-        }
-
-        private int SpareBonus(int frameIndex)
-        {
-            return _rolls[frameIndex + 2];
-        }
-
-        private int StrikeBonus(int frameIndex)
-        {
-            return _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
-        }
-
-        private bool IsStrike(int frameIndex)
-        {
-            return _rolls[frameIndex] == 10;
-        }
-
-        private bool IsSpare(int frameIndex)
-        {
-            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+            return 0;
         }
     }
 }
